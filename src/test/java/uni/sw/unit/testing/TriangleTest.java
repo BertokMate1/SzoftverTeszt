b9674a7946403b9a101df1915b70e7d79f2a7971
Máté <weckerq1337@gmail.com>
2024-09-26 13:16:38 +0200

    some more test

--- src/test/java/uni/sw/unit/testing/TriangleTest.java
+++ src/test/java/uni/sw/unit/testing/TriangleTest.java
@@ -1,7 +1,10 @@
 package uni.sw.unit.testing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class TriangleTest {
@@ -23,6 +26,7 @@ public class TriangleTest {
     public void testIsosceles(){
 
         Triangle triangle = new Triangle(3,3,5);
+
         assertTrue(triangle.isIsosceles());
 
     }
@@ -37,4 +41,49 @@ public class TriangleTest {
 
     }
 
+    @Disabled
+    @Test
+    public void testisRightAngeled(){
+
+        Triangle triangle = new Triangle(0, 0, 0);
+
+    }
+
+    @Test
+    public void testPerimeter(){
+
+        Triangle triangle = new Triangle(100, 251, 178);
+
+        assertEquals(triangle.getPerimeter(), 529, "calculated perimeter doesnt match expected value");
+
+    }
+
+    @Disabled
+    @Test
+    public void testArea(){
+
+        Triangle triangle = new Triangle(100, 251, 178);
+
+        assertEquals(triangle.getArea(), 529, "calculated perimeter doesnt match expected value");
+
+    }
+
+    @Test
+    public void testNotConctructableTriangle(){
+
+        Exception exception1 = assertThrows(ArithmeticException.class, () -> {
+            new Triangle(3, 4, 7);
+        });
+
+        assertTrue(exception1.getMessage().contains("The triangle cannot be constructed from the the numbers"));
+
+            assertThrows(ArithmeticException.class, () -> {
+                new Triangle(3, 7, 4);
+        });
+
+            assertThrows(ArithmeticException.class, () -> {
+                new Triangle(7, 3, 4);
+        });
+
+    }
 }
