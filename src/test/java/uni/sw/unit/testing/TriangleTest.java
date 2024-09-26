9381c73b28397420bd27fc48076857087b6cc8e3
Máté <weckerq1337@gmail.com>
2024-09-26 13:30:59 +0200

    some more test

--- src/test/java/uni/sw/unit/testing/TriangleTest.java
+++ src/test/java/uni/sw/unit/testing/TriangleTest.java
@@ -1,18 +1,43 @@
 package uni.sw.unit.testing;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class TriangleTest {
 
+    @BeforeAll
+    public static void setup(){
+        System.out.println("BeforeAll demo setup test execution");
+    }
+
+    @AfterAll
+    public static void teardown(){
+        System.out.println("AfterAll demo tear down test execution");
+    }
+
+    @BeforeEach
+    public void initTest(){
+        System.out.println("BeforeEach demo init test case ");
+    }
+
+    @AfterEach
+    public void tearDownTest(){
+        System.out.println("AfterEach demo tear down test case ");
+    }
+
 
     @Test
     public void testGenericTriangle(){
 
+        System.out.println("Testing generic triangle");
         Triangle triangle = new Triangle(3,4,6);
 
         assertTrue(triangle instanceof Triangle);
@@ -24,7 +49,8 @@ public class TriangleTest {
 
     @Test
     public void testIsosceles(){
-
+        
+        System.out.println("Testing isosceles triangle");
         Triangle triangle = new Triangle(3,3,5);
 
         assertTrue(triangle.isIsosceles());
@@ -84,6 +110,6 @@ public class TriangleTest {
             assertThrows(ArithmeticException.class, () -> {
                 new Triangle(7, 3, 4);
         });
-
     }
+
 }
